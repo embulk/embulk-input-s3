7a8b0d86278252181eabe76c63cb27c8d173dbf4
Sadayuki Furuhashi <frsyuki@gmail.com>
2015-06-26 13:59:22 -0700

    Add embulk-input-riak_cs

---
+++ embulk-input-riak_cs/src/main/java/org/embulk/input/riak_cs/RiakCsFileInputPlugin.java
@@ -0,0 +1,47 @@
+package org.embulk.input.riak_cs;
+
+import com.amazonaws.ClientConfiguration;
+import com.amazonaws.services.s3.AmazonS3Client;
+import org.embulk.config.Config;
+import org.embulk.config.ConfigInject;
+import org.embulk.input.s3.AbstractS3FileInputPlugin;
+
+public class RiakCsFileInputPlugin
+        extends AbstractS3FileInputPlugin
+{
+    public interface RiakCsPluginTask
+            extends PluginTask
+    {
+        @Config("endpoint")
+        public String getEndpoint();
+    }
+
+    @Override
+    protected Class<? extends PluginTask> getTaskClass()
+    {
+        return RiakCsPluginTask.class;
+    }
+
+    @Override
+    protected AmazonS3Client newS3Client(PluginTask task)
+    {
+        RiakCsPluginTask t = (RiakCsPluginTask) task;
+
+        AmazonS3Client client = super.newS3Client(t);
+
+        client.setEndpoint(t.getEndpoint());
+
+        return client;
+    }
+
+    @Override
+    protected ClientConfiguration getClientConfiguration(PluginTask task)
+    {
+        RiakCsPluginTask t = (RiakCsPluginTask) task;
+
+        ClientConfiguration config = super.getClientConfiguration(t);
+        config.setSignerOverride("S3SignerType");
+
+        return config;
+    }
+}
---
+++ embulk-input-s3/src/main/java/org/embulk/input/s3/S3FileInputPlugin.java
@@ -0,0 +1,39 @@
+package org.embulk.input.s3;
+
+import com.google.common.base.Optional;
+import com.amazonaws.services.s3.AmazonS3Client;
+import org.embulk.config.Config;
+import org.embulk.config.ConfigDefault;
+import org.embulk.input.s3.AbstractS3FileInputPlugin;
+
+public class S3FileInputPlugin
+        extends AbstractS3FileInputPlugin
+{
+    public interface S3PluginTask
+            extends PluginTask
+    {
+        @Config("endpoint")
+        @ConfigDefault("null")
+        public Optional<String> getEndpoint();
+    }
+
+    @Override
+    protected Class<? extends PluginTask> getTaskClass()
+    {
+        return S3PluginTask.class;
+    }
+
+    @Override
+    protected AmazonS3Client newS3Client(PluginTask task)
+    {
+        S3PluginTask t = (S3PluginTask) task;
+
+        AmazonS3Client client = super.newS3Client(t);
+
+        if (t.getEndpoint().isPresent()) {
+            client.setEndpoint(t.getEndpoint().get());
+        }
+
+        return client;
+    }
+}
