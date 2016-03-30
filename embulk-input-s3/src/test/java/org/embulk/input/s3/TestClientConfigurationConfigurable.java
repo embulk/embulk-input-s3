76140c0421808d13420232c632817bf08fdb708d
Civitaspo <civitaspo@gmail.com>
2016-03-30 18:12:17 +0900

    Remove unuse code from TestClientConfigurationConfigurable.java

--- embulk-input-s3/src/test/java/org/embulk/input/s3/TestClientConfigurationConfigurable.java
+++ embulk-input-s3/src/test/java/org/embulk/input/s3/TestClientConfigurationConfigurable.java
@@ -5,60 +5,28 @@ import com.amazonaws.Protocol;
 import org.embulk.EmbulkTestRuntime;
 import org.embulk.config.ConfigException;
 import org.embulk.config.ConfigSource;
-import org.embulk.config.TaskReport;
-import org.embulk.config.TaskSource;
 import org.embulk.spi.Exec;
-import org.embulk.spi.FileInputRunner;
-import org.embulk.spi.InputPlugin;
-import org.embulk.spi.Schema;
-import org.embulk.spi.TestPageBuilderReader;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.junit.Test;
 
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
-import java.util.List;
 
 import static org.embulk.input.s3.TestS3FileInputPlugin.parserConfig;
 import static org.embulk.input.s3.TestS3FileInputPlugin.schemaConfig;
-import static org.junit.Assert.*;
-import static org.junit.Assume.assumeNotNull;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Created by takahiro.nakayama on 3/30/16.
  */
 public class TestClientConfigurationConfigurable
 {
-    private static String EMBULK_S3_TEST_BUCKET;
-    private static String EMBULK_S3_TEST_ACCESS_KEY_ID;
-    private static String EMBULK_S3_TEST_SECRET_ACCESS_KEY;
-    private static final String EMBULK_S3_TEST_PATH_PREFIX = "embulk_input_s3_test";
-
-    /*
-     * This test case requires environment variables:
-     *   EMBULK_S3_TEST_BUCKET
-     *   EMBULK_S3_TEST_ACCESS_KEY_ID
-     *   EMBULK_S3_TEST_SECRET_ACCESS_KEY
-     * If the variables not set, the test case is skipped.
-     */
-    @BeforeClass
-    public static void initializeConstantVariables()
-    {
-        EMBULK_S3_TEST_BUCKET = System.getenv("EMBULK_S3_TEST_BUCKET");
-        EMBULK_S3_TEST_ACCESS_KEY_ID = System.getenv("EMBULK_S3_TEST_ACCESS_KEY_ID");
-        EMBULK_S3_TEST_SECRET_ACCESS_KEY = System.getenv("EMBULK_S3_TEST_SECRET_ACCESS_KEY");
-        assumeNotNull(EMBULK_S3_TEST_BUCKET, EMBULK_S3_TEST_ACCESS_KEY_ID, EMBULK_S3_TEST_SECRET_ACCESS_KEY);
-    }
-
     @Rule
     public EmbulkTestRuntime runtime = new EmbulkTestRuntime();
 
     private S3FileInputPlugin plugin;
     private ConfigSource config;
-    private FileInputRunner runner;
-    private TestPageBuilderReader.MockPageOutput output;
 
     @Before
     public void createResources()
@@ -66,11 +34,9 @@ public class TestClientConfigurationConfigurable
         plugin = new S3FileInputPlugin();
         config = runtime.getExec().newConfigSource()
                 .set("type", "s3")
-                .set("bucket", EMBULK_S3_TEST_BUCKET)
-                .set("path_prefix", EMBULK_S3_TEST_PATH_PREFIX)
+                .set("bucket", "dummy")
+                .set("path_prefix", "dummy")
                 .set("parser", parserConfig(schemaConfig()));
-        runner = new FileInputRunner(runtime.getInstance(S3FileInputPlugin.class));
-        output = new TestPageBuilderReader.MockPageOutput();
     }
 
     @Test
