76be5b93c6c56b131530b4afe03e8e91645d9e24
Uchino Jun <uchino@sansan.com>
2017-12-18 22:29:44 +0900

    refactored a little

--- embulk-input-s3/src/main/java/org/embulk/input/s3/AbstractS3FileInputPlugin.java
+++ embulk-input-s3/src/main/java/org/embulk/input/s3/AbstractS3FileInputPlugin.java
@@ -368,10 +368,11 @@ public abstract class AbstractS3FileInputPlugin
                 S3Object obj = client.getObject(request);
                 return new ResumableInputStream(obj.getObjectContent(), new S3InputStreamReopener(client, request, obj.getObjectMetadata().getContentLength()));
             } catch (AmazonS3Exception ex) {
+                 int statusCode = ex.getStatusCode();
                 // HTTP 403 errors caused by a glacier object.
-                if (ex.getStatusCode() == 403 && "InvalidObjectState".equalsIgnoreCase(ex.getErrorCode())) {
+                if (statusCode == 403 && "InvalidObjectState".equalsIgnoreCase(ex.getErrorCode())) {
                     if (this.skip_glacier_object) {
-                        log.warn("Skipped \"s3://{}/{}\" that stored at Gracier. status code: {}", this.bucket, request.getKey(), ex.getStatusCode());
+                        log.warn("Skipped \"s3://{}/{}\" that stored at Gracier. status code: {}", this.bucket, request.getKey(), statusCode);
                         return null;
                     } else {
                         throw new ConfigException(ex);
