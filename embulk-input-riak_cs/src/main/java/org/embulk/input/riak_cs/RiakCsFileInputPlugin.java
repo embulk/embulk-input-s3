d4eb589c950d7449c69769156fe4333f19f51986
Huy Le <huy.lenq@gmail.com>
2017-12-22 14:38:17 +0700

    Refactor: favor AWS client builder over the deprecated mutable constructor

--- embulk-input-riak_cs/src/main/java/org/embulk/input/riak_cs/RiakCsFileInputPlugin.java
+++ embulk-input-riak_cs/src/main/java/org/embulk/input/riak_cs/RiakCsFileInputPlugin.java
@@ -1,10 +1,15 @@
 package org.embulk.input.riak_cs;
 
 import com.amazonaws.ClientConfiguration;
+import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3Client;
 import org.embulk.config.Config;
-import org.embulk.config.ConfigInject;
 import org.embulk.input.s3.AbstractS3FileInputPlugin;
+
+import static com.amazonaws.client.builder.AwsClientBuilder.EndpointConfiguration;
+import static com.amazonaws.services.s3.AmazonS3Client.*;
+import static com.amazonaws.util.AwsHostNameUtils.parseRegion;
+import static com.amazonaws.util.RuntimeHttpUtils.toUri;
 
 public class RiakCsFileInputPlugin
         extends AbstractS3FileInputPlugin
@@ -23,15 +28,21 @@ public class RiakCsFileInputPlugin
     }
 
     @Override
-    protected AmazonS3Client newS3Client(PluginTask task)
+    protected AmazonS3 newS3Client(PluginTask task)
     {
         RiakCsPluginTask t = (RiakCsPluginTask) task;
 
-        AmazonS3Client client = super.newS3Client(t);
-
-        client.setEndpoint(t.getEndpoint());
-
-        return client;
+        return super
+                .defaultS3ClientBuilder(task)
+                .withEndpointConfiguration(new EndpointConfiguration(
+                        t.getEndpoint(),
+                        // Although client will treat endpoint's region as the signer region
+                        // if we left this as null, but such that behaviour is undocumented,
+                        // so it is explicitly calculated here for future-proofing.
+                        parseRegion(
+                                toUri(t.getEndpoint(), getClientConfiguration(task)).getHost(),
+                                S3_SERVICE_NAME)))
+                .build();
     }
 
     @Override
