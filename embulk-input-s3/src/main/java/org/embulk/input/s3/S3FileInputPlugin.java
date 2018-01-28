c4ace6618c2edc0fca8922522d38d53a77b25f18
Huy Le <huy.lenq@gmail.com>
2018-01-28 15:46:21 +0700

    Add some comments to be clear on an intent of backward compatibility

--- embulk-input-s3/src/main/java/org/embulk/input/s3/S3FileInputPlugin.java
+++ embulk-input-s3/src/main/java/org/embulk/input/s3/S3FileInputPlugin.java
@@ -40,6 +40,7 @@ public class S3FileInputPlugin
 
         AmazonS3ClientBuilder builder = super.defaultS3ClientBuilder(t);
 
+        // Favor the `endpoint` configuration, then `region`, if both are absent then `s3.amazonaws.com` will be used.
         if (endpoint.isPresent()) {
             if (region.isPresent()) {
                 log.warn("Either configure endpoint or region, " +
@@ -49,6 +50,19 @@ public class S3FileInputPlugin
         } else if (region.isPresent()) {
             builder.setRegion(region.get());
         } else {
+            // This is to keep the AWS SDK upgrading to 1.11.x to be backward compatible with old configuration.
+            //
+            // On SDK 1.10.x, when neither endpoint nor region is set explicitly, the client's endpoint will be by
+            // default `s3.amazonaws.com`. And for pre-Signature-V4, this will work fine as the bucket's region
+            // will be resolved to the appropriate region on server (AWS) side.
+            //
+            // On SDK 1.11.x, a region will be computed on client side by AwsRegionProvider and the endpoint now will
+            // be region-specific `<region>.s3.amazonaws.com` and might be the wrong one.
+            //
+            // So a default endpoint of `s3.amazonaws.com` when both endpoint and region configs are absent are
+            // necessary to make old configurations won't suddenly break. The side effect is that this will render
+            // AwsRegionProvider useless. And it's worth to note that Signature-V4 won't work with either versions with
+            // no explicit region or endpoint as the region (inferrable from endpoint) are necessary for signing.
             builder.setEndpointConfiguration(new EndpointConfiguration("s3.amazonaws.com", null));
         }
 
